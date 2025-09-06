f7d2470f7e6ec267d16aed4301faafe6553c9c62
Hatem-Idris <hatem.h.idris@gmail.com>
2025-09-06 22:14:08 +0300

    portofolio (React)

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="z-[1] border-b border-grayDark/60">
       <div className="mx-auto max-w-container px-6 md:px-8 py-6 flex justify-center md:justify-start">
-        <h1 className="text-xl md:text-2xl font-bold tracking-tight">adamkeyes</h1>
+        <h1 className="text-xl md:text-2xl font-bold tracking-tight">adamkeyess</h1>
       </div>
     </header>
   )
